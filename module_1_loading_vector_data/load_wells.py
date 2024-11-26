9ab7bfd6b87c9692946a5141460202cc76cf3dc4
Philip Ahrens <peahrens90@gmail.com>
2024-11-26 17:35:55 -0600

    Add function to load pipelines data to PostgreSQL. Add Results.png with altered colors of pipelines and wells

--- module_1_loading_vector_data/load_wells.py
+++ module_1_loading_vector_data/load_wells.py
@@ -2,9 +2,9 @@ import geopandas as gpd
 from sqlalchemy import create_engine
 
 # Database connection parameters
-DB_NAME = "your_database_name_here"
-DB_USER = "your_pg_username_here"
-DB_PASSWORD = "your_pg_password_here"
+DB_NAME = "postgres"
+DB_USER = "postgres"
+DB_PASSWORD = "postgres"
 DB_HOST = "localhost"
 DB_PORT = "5432"
 
@@ -12,7 +12,7 @@ DB_PORT = "5432"
 def load_wells_to_postgres():
     # Read the filtered GeoPackage data
     gdf = gpd.read_file(
-        "C:/file/path/to/your/downloaded/data/here/OGIM_v2.5.1.gpkg",
+        "/Users/philipahrens/Downloads/OGIM_v2.5.1.gpkg",
         layer='Oil_and_Natural_Gas_Wells', # layer name
         mask=None,
         where="STATE_PROV = 'NEW MEXICO' AND OGIM_STATUS = 'PRODUCING'" # filter criteria
